596f8dc842fc77d104473e3b9b250e9c00d6df17
Luca <luca-marconi@hotmail.com>
2020-05-07 10:46:29 +0200

    Laravel Mix

---
+++ resources/views/layouts/user-layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield('titolo')</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+    <body>
+        <header>
+            @include('partials.header')
+        </header>
+        <main>
+            @yield('content')
+        </main>
+        <footer>
+            @include('partials.footer')
+        </footer>
+        @yield('script')
+    </body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<div class="header">
+    <div class="logo">
+        <img src="https://d33wubrfki0l68.cloudfront.net/81fa290218cedc39d32be7b1f1aba58a25829ebe/0a05f/images/logo--full.svg" alt="">
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#">Articles</a></li>
+            <li><a href="#">Guide</a></li>
+            <li><a href="#">Book</a></li>
+            <li><a href="#">Event</a></li>
+            <li><a href="#">Membership</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Newsletter</a></li>
+            <li><a href="#">Podcast</a></li>
+        </ul>
+    </nav>
+</div>
